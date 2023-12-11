7fea6b2d27b0f444049f63cd69c9b50343371d5f
vintech05 <kelanjutannyakelvin@gmail.com>
2023-12-11 21:17:00 +0700

    fixed bug when sending empty email

--- dist/script.js
+++ dist/script.js
@@ -3,15 +3,16 @@ const email = document.querySelector('input[name="email"]');
 const error =  document.querySelector('.error');
 
 
-form.addEventListener('submit', () => {
+form.addEventListener('submit', (e) => {
     if (email.value === undefined || email.value === '') {
         error.style.display = 'block';
         email.style.borderColor = 'red';
         error.textContent = 'Please provide an email address';
+        e.preventDefault();
     } else {
+        email.input.value == '';
         email.style.borderColor = 'green';
         error.style.display = 'none';
     }
-
-    e.preventDefault();
+    
 })
